fe23d974d347108f223817208fd248881424a988
jevontrei <160815182+jevontrei@users.noreply.github.com>
2026-03-14 08:57:39 +1000

    add setup scripp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dotfiles
+
+this is now the SSOT! my zshrc and bashrc files are now symlinks that point to this repo
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# set up symlinks so that this repo becomes the SSOT
+ln -sf ~/repos/dotfiles/.bashrc ~/.bashrc 
+ln -sf ~/repos/dotfiles/.zshrc ~/.zshrc 
